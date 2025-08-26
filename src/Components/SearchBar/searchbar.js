803f522b0cbc3eb3b2a9a5797624c60506459572
Kierath <kierath_dhugga@hotmail.co.uk>
2025-08-26 14:34:33 +0100

    changed h2 title

--- src/Components/SearchBar/searchbar.js
+++ src/Components/SearchBar/searchbar.js
@@ -29,7 +29,7 @@ function SearchBar({ token, onAdd }) {
 
   return (
   <div className="SearchBarContainer">
-    <h2>Which song do you want to add?</h2>
+    <h2>Which song do you want to add to your selected playlist?</h2>
     <div className="SearchControls">
       <input
         type="text"
